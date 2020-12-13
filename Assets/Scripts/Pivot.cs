3b06f080cc234be2e5cff6d5861f7eef3d4d6b8b
cmlee0913 <43628924+cmlee0913@users.noreply.github.com>
2020-12-14 01:36:43 +0900

    12.14
    Player Move

--- Assets/Scripts/Pivot.cs
+++ Assets/Scripts/Pivot.cs
@@ -18,6 +18,7 @@ public class Pivot : MonoBehaviour
         Rotate();
     }
 
+    /*플레이어 머리 위로 위치 고정*/
     private void Pos()
     {
         transform.position = Player.transform.position + (Vector3.up * 1.5f);
@@ -28,18 +29,21 @@ public class Pivot : MonoBehaviour
         Vector3 Angles = transform.rotation.eulerAngles;
         float MouseX = Input.GetAxis("Mouse X");
         float MouseY = Input.GetAxis("Mouse Y");
-        float eulerAnglesX = Angles.x - MouseY;
+        float eulerAnglesX = Angles.x - MouseY; // 최대 카메라 각도 설정을 위한 변수
 
+        /* 회전 각도 최대 범위 설정 */
         if (eulerAnglesX < 180)
         {
-            eulerAnglesX = Mathf.Clamp(eulerAnglesX, -1, 70);
+            eulerAnglesX = Mathf.Clamp(eulerAnglesX, -1, 70); // -1도의 이유는 수평 부근에서의 멈춤을 막기위해
         }
         else if (eulerAnglesX >= 180)
         {
-            eulerAnglesX = Mathf.Clamp(eulerAnglesX, 340, 361);
+            eulerAnglesX = Mathf.Clamp(eulerAnglesX, 340, 361); // 1도의 이유는 위와 같음
         }
 
         //transform.eulerAngles += new Vector3(-MouseY, MouseX, 0) * 3f;
+
+        /* 마우스 움직임에 따라 CamSpot 회전 값 변경 */
         transform.rotation = Quaternion.Euler(eulerAnglesX, Angles.y + MouseX, Angles.z);
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Pivot;
+    private bool isMove = false;
+
+    private void Start()
+    {
+        Pivot = GameObject.FindWithTag("Pivot");
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float Horizontal = Input.GetAxisRaw("Horizontal");
+        float Vertical = Input.GetAxisRaw("Vertical");
+        float Speed = 5f;
+
+        /* 대쉬 유무 판별 */
+        if (Input.GetKey(KeyCode.LeftShift) == true)
+        {
+            Speed = 10f;
+        }
+        else if (Input.GetKey(KeyCode.LeftShift) == false)
+        {
+            Speed = 5f;
+        }
+        /* 방향키 입력 유무 판별 */
+        if (Horizontal != 0 || Vertical != 0)
+        {
+            isMove = true;
+        }
+
+        /* 카메라 스팟의 전후방 좌우 방향을 기준 */
+        Vector3 moveVertical = new Vector3(Pivot.transform.forward.x,
+                                           0, // 이동 방향을 2차원적으로 고정 시키기 위해
+                                           Pivot.transform.forward.z).normalized;
+        Vector3 moveHorizontal = new Vector3(Pivot.transform.right.x,
+                                             0, // 이동 방향을 2차원적으로 고정 시키기 위해
+                                             Pivot.transform.right.z).normalized;
+        Vector3 move = moveVertical * Vertical + moveHorizontal * Horizontal;
+
+        if (isMove == true)
+        {
+            transform.forward = move; // 플레이어의 시선 방향이 방향키 입력 방향과 일치하도록 회전
+            transform.position += move * Time.deltaTime * Speed; // 키보드 입력 시 camSpot의 전방 또는 좌우으로 이동
+            isMove = false; // 시선 방향 고정
+        }
+    }
+}
